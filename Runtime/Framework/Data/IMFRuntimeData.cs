085f3fa19831c86c9d6da59fea36287634202320
Reguluz <154496676@qq.com>
2022-02-07 23:26:41 +0800

    更新标准数据帧前后动作

--- Runtime/Framework/Data/IMFRuntimeData.cs
+++ Runtime/Framework/Data/IMFRuntimeData.cs
@@ -3,7 +3,9 @@ namespace Moonflow.Core
     public interface IMFRuntimeData
     {
         public void OnInitial();
+        public void BeforeTickUpdate();
         public void OnTickUpdate();
+        public void LateTickUpdate();
         public void OnEnd();
     }
 }
